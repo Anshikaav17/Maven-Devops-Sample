0cc450146e68a2a6a47ef825f3e378c8fce1a6ab
Anshikaav17 <2021wa86155@wilp.bits-pilani.ac.in>
2025-07-13 00:30:01 +0530

    Update apptest.java

--- src/java/com/example/apptest.java
+++ src/java/com/example/apptest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
  
-public class AppTest {
+public class apptest {
     @Test
     public void testApp() {
         assertTrue(true);
