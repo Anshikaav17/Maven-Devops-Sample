5c07ca528549741d8c4580c4288d9b59b6b563bb
Anshikaav17 <2021wa86155@wilp.bits-pilani.ac.in>
2025-07-13 00:39:23 +0530

    Update app.java

--- src/main/java/com /example /app.java
+++ src/main/java/com /example /app.java
@@ -2,6 +2,6 @@ package com.example;
  
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello, JAVA!");
+        System.out.println("Hello, Hi JAVA!");
     }
 }
